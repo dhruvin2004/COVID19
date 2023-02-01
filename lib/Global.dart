91b6af17e841b3e3ef02aa14becb71bc27da484b
dhruvin <dhruvinpandav999@gmail.com>
2023-02-01 09:38:55 +0530

    DP | 1/2/23 9:40 AM

--- lib/Global.dart
+++ lib/Global.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
 class Global
 {
-
+   static  TextEditingController countryController = TextEditingController();
+   static  TextEditingController stateController = TextEditingController();
+   static  TextEditingController cityController = TextEditingController();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'dart:convert';
 
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
 
+import 'Global.dart';
 import 'modal.dart';
 
-
-
-
-void main()
-{
+void main() {
   runApp(
     MyApp(),
   );
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -26,14 +24,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
-
-
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -45,13 +41,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   HttpService httpService = HttpService();
 
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     httpService.getCovidData();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,20 +55,44 @@ class _HomePageState extends State<HomePage> {
       body: FutureBuilder(
         future: httpService.getCovidData(),
         builder: (context, snapshot) {
-           if (snapshot.hasData) {
-             List<CovidData> data = snapshot.data;
-             return Column(
-               children: [
-                 Text("${data[0].active}"),
-                 Text(""),
-               ],
-             );
+          if (snapshot.hasData) {
+            List<CountryData> data = snapshot.data;
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Country",
+                    style: GoogleFonts.lato(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  CustomDropdown.search(
+                    listItemStyle: GoogleFonts.lato(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
+                    hintText: 'Select Country',
+                    items: [
+                      ...data!
+                          .map((CountryData e) => e.country.toString())
+                          .toList(),
+                    ],
+                    controller: Global.countryController,
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
-           else {
-             return const Center(
-               child: CircularProgressIndicator(),
-             );
-           }
         },
       ),
     );
@@ -81,12 +101,12 @@ class _HomePageState extends State<HomePage> {
 
 class HttpService {
   Future getCovidData() async {
-    Response response = await get(Uri.parse('https://disease.sh/v3/covid-19/countries'));
+    Response response =
+        await get(Uri.parse('https://api.covid19api.com/countries'));
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
-      List covid = data.map((e) => CovidData.fromJson(e)).toList();
+      List covid = data.map((e) => CountryData.fromJson(e)).toList();
       return covid;
-
     } else {
       print(response.statusCode);
       throw "Unable to retrieve posts.";
